f3f0472a1782706df1a8a0ecf26a6079b4166578
Pia Westerbergh <Pia Westerbergh>
2016-12-01 09:55:11 +0100

    Line

--- Geometri/src/Line.java
+++ Geometri/src/Line.java
@@ -5,19 +5,21 @@ import java.awt.*;
  * class for lines
  */
 public class Line extends Shape {
-    private int x1;
-    private int y1;
-    private int x2;
-    private int y2;
+    //private int x1;
+    //private int y1;
+    //private int x2;
+    //private int y2;
+    private boolean up;
 
     public Line(int x1, int y1, int x2, int y2, Color c) throws IllegalPositionException {
-        this.x1 = x1;
-        this.y1 = y1;
+        if(y1<y2){
+            up=false;
+        }else{
+            up=true;
+        }
         this.color = c;
-        this.x2 = x2;
-        this.y2 = y2;
-        this.x = setX();
-        this.y = setY();
+        this.x = setX(x1,x2);
+        this.y = setY(y1,y2);
         width = Math.abs(x1 - x2);
         height = Math.abs(y1 - y2);
         if (checkIllegal() || x2 < 0 || y2 < 0) {
@@ -27,18 +29,8 @@ public class Line extends Shape {
         calcCircumference();
     }
 
-    public Line(GeometricalForm f1, GeometricalForm f2, Color c) {
-        this.x1 = f1.getX();
-        this.y1 = f1.getY();
-        this.color = c;
-        this.x2 = f2.getX();
-        this.y2 = f2.getY();
-        this.x = setX();
-        this.y = setY();
-        width = Math.abs(x1 - x2);
-        height = Math.abs(y1 - y2);
-        calcArea();
-        calcCircumference();
+    public Line(GeometricalForm f1, GeometricalForm f2, Color c) throws IllegalPositionException {
+        this(f1.getX(), f1.getY(), f2.getX(), f2.getY(), c);
     }
 
     /** {@inheritDoc} */
@@ -51,14 +43,14 @@ public class Line extends Shape {
         circumference = (int) Math.round(Math.sqrt(height * height + width * width));
     }
 
-    private int setX() {
+    private int setX(int x1, int x2) {
         if (x2 < x1) {
             return x2;
         }
         return x1;
     }
 
-    private int setY() {
+    private int setY(int y1, int y2) {
         if (y2 < y1) {
             return y2;
         }
@@ -68,10 +60,6 @@ public class Line extends Shape {
     /** {@inheritDoc} */
     public void move(int dx, int dy) throws IllegalPositionException {
         super.move(dx, dy);
-        x1 += dx;
-        x2 += dx;
-        y1 += dy;
-        y2 += dy;
     }
 
     /** {@inheritDoc} */
@@ -84,21 +72,29 @@ public class Line extends Shape {
     /** draws the line */
     public void fill(Graphics g) {
         g.setColor(color);
-        g.drawLine(x1, y1, x2, y2);
+        if(up){
+            g.drawLine(x, y+height, x+width, y);
+        }else{
+            g.drawLine(x, y, x+width, y+height);
+        }
     }
 
     /** {@inheritDoc} */
     @Override
     public boolean equals(Object o) {
         if (o instanceof Line && super.equals(o)) {
-            return true;
+            Line line= (Line) o;
+            if(this.up==line.up){
+                return true;
+            }
         }
         return false;
     }
 
     /** {@inheritDoc} */
     public int hashCode() {
-        return super.hashCode();
+        if (up){return (super.hashCode()+1)*17;}
+        else{return super.hashCode();}
     }
 
 }
